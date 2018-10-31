7300ff3ee40dedd9a15a2a3b21fab2d5627223fa
Feng George Yu <fyu@cianna-office.local>
2018-10-31 10:11:27 -0400

    init.el

--- init.el
+++ init.el
@@ -43,10 +43,24 @@
 (require 'init-folding)
 ;;---javascript----
 (require 'init-javascript)
-
+;;---sql---
 (require 'init-sql)
-
+;;---whitespace---
 (require 'init-whitespace)
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (whitespace-cleanup-mode sqlup-mode sqlformat add-node-modules-path skewer-mode js-comint prettier-js typescript-mode coffee-mode js2-mode json-mode origami company-quickhelp company magithub github-clone bug-reference-github yagist git-messenger git-commit fullframe magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed anaconda-mode pip-requirements command-log-mode scratch diminish wgrep zenburn-theme tabbar sr-speedbar powerline monokai-theme markdown-mode atom-one-dark-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
