33edd5beed2d901560bb2b1e65c50f0a5d7ac223
Feng Yu <fyu@ysu.edu>
2019-09-28 08:25:49 -0400

    local

--- init.el
+++ init.el
@@ -49,3 +49,17 @@
 (require 'init-whitespace)
 ;;---ssh---
 (require 'init-ssh)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ssh whitespace-cleanup-mode sqlup-mode sqlformat add-node-modules-path skewer-mode js-comint prettier-js typescript-mode coffee-mode js2-mode json-mode origami company-quickhelp magithub github-clone bug-reference-github yagist git-messenger fullframe magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed company-anaconda anaconda-mode pip-requirements sr-speedbar tabbar borland-blue-theme atom-one-dark-theme zenburn-theme monokai-theme powerline command-log-mode scratch diminish wgrep))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
