b6334745571a594a06e40d0fa344c0931c99ac02
George Yu <fyu@ysu.edu>
2019-09-28 18:06:26 -0400

    revise imenu

--- site-lisp/init-imenu-list.el
+++ site-lisp/init-imenu-list.el
@@ -1,11 +1,17 @@
 (require-package 'imenu-list)
 ;; https://github.com/bmag/imenu-list
 
-(global-set-key (kbd "C-c b") #'imenu-list-smart-toggle)
+;; automatically display tag on start
+;;(imenu-list-minor-mode)
+;;;;---open tag window---
+(global-set-key (kbd "C-c b") #'imenu-list-minor-mode)
+;;(global-set-key (kbd "C-c b") #'imenu-list-smart-toggle)
 ;; (global-set-key (kbd "<C-'>") 'imenu-list-smart-toggle)
 ;; (define-key global-map [(ctl ')] 'imenu-list-smart-toggle)
 ;;(define-key global-map "\C-'" 'imenu-list-smart-toggle)
-(setq imenu-list-focus-after-activation t)
-(setq imenu-list-auto-resize t)
+;;;;---set the focus to the *Ilist* window---
+;;(setq imenu-list-focus-after-activation t)
+;;;;---automatically resized every time the *Ilist* buffer is updated---
+;;(setq imenu-list-auto-resize t)
 (setq imenu-list-after-jump-hook nil)
 (provide 'init-imenu-list)
